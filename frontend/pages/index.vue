9ad0470b7e541a8791bf6416ce751a355d6e5017
Nestor Medina <medinanestor86@gmail.com>
2021-07-03 23:33:10 -0300

    add middleware, pages and plugins

---
+++ frontend/pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="page-wrapper">
+    <Menu></Menu>
+    <ImageHero></ImageHero>
+    <ExampleList></ExampleList>
+    <Footer></Footer>
+  </div>
+
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue';
+import ImageHero from '~/components/home/image-hero.vue';
+import ExampleList from '~/components/home/example-list.vue';
+import Menu from '~/layouts/menu.vue';
+import Footer from '~/layouts/footer.vue';
+
+export default {
+  components: {
+    Logo, Menu, ImageHero, Footer, ExampleList
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ frontend/pages/user/register/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="page-wrapper">
+    <Menu></Menu>
+    <Register></Register>
+    <Footer></Footer>
+  </div>
+
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue';
+import Menu from '~/layouts/menu.vue';
+import Footer from '~/layouts/footer.vue';
+import Register from "~/components/users/Register";
+
+export default {
+  components: {
+    Logo, Menu, Footer, Register
+  }
+}
+</script>
+
+<style>
+</style>
